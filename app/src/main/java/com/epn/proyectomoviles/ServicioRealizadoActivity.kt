0579d36811924263e5ad7ca537cc67ddaea496d9
KatherinL <49216328+KatherinL@users.noreply.github.com>
2019-12-04 09:19:49 -0500

    commit

---
+++ app/src/main/java/com/epn/proyectomoviles/ServicioRealizadoActivity.kt
@@ -0,0 +1,28 @@
+package com.epn.proyectomoviles
+
+<<<<<<< Updated upstream
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+=======
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+>>>>>>> Stashed changes
+
+class ServicioRealizadoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_servicio_realizado)
+    }
+<<<<<<< Updated upstream
+=======
+
+    fun service(view: View){
+
+        //var intent : Intent = Intent(this, Activity::class.java)
+        //startActivity(intent)
+    }
+>>>>>>> Stashed changes
+}
