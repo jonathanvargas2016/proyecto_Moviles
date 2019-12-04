07c3fc747131448691bb0d6fcbefcfc9df3b442e
KatherinL <49216328+KatherinL@users.noreply.github.com>
2019-12-04 09:31:31 -0500

    Update ServicioRealizadoActivity.kt

--- app/src/main/java/com/epn/proyectomoviles/ServicioRealizadoActivity.kt
+++ app/src/main/java/com/epn/proyectomoviles/ServicioRealizadoActivity.kt
@@ -1,14 +1,8 @@
 package com.epn.proyectomoviles
 
-<<<<<<< Updated upstream
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-=======
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
->>>>>>> Stashed changes
 
 class ServicioRealizadoActivity : AppCompatActivity() {
 
@@ -16,13 +10,10 @@ class ServicioRealizadoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_servicio_realizado)
     }
-<<<<<<< Updated upstream
-=======
 
     fun service(view: View){
 
         //var intent : Intent = Intent(this, Activity::class.java)
         //startActivity(intent)
     }
->>>>>>> Stashed changes
 }
